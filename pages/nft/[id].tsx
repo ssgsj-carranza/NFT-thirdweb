903dc1ef45f5746c118ddd8f12a718548cb2f767
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-04-01 11:17:50 -0500

    adding styling to nft page

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function NFTDropPage() {
   return (
-    <div>NFTDropPage</div>
+    <div className="flex h-screen flex-col">
+        {/* left side */}
+        <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
+            <div>
+                <h1>Test</h1>
+            </div>
+        </div>
+
+        {/* right side */}
+        <div>
+
+        </div>
+    </div>
   )
 }
 
