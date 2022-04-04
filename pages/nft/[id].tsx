4092f0ae6b0833656b4e0a948cfbeb51a3addf42
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-04-04 11:48:05 -0500

    adding screen responsiveness to layout

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function NFTDropPage() {
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
         {/* left side */}
-        <div className="bg-gradient-to-br from-cyan-800 to-rose-500">
+        <div className="bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4">
             <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
                 <div className="bg-gradient-to-br from-orange-200 to-purple-600 p-2 rounded-xl">
                     <img className="w-44 rounded-xl object-cover lg:h-96 lg:w-72" 
@@ -12,7 +12,7 @@ function NFTDropPage() {
                         alt="" 
                     />
                 </div>
-                <div>
+                <div className='text-center p-5 space-y-2'>
                     <h1 className="text-4xl font-bold text-white">NFTs</h1>
                     <h2 className="text-xl text-gray-300">A collection on NFTs not screenshotted</h2>
                 </div>
