6dddddbac1fb888151bdd67effa447f9745177e1
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-04-04 12:09:44 -0500

    added styling to header and sign in button, continuing styling

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -20,11 +20,11 @@ function NFTDropPage() {
         </div>
 
         {/* right side */}
-        <div>
+        <div className="flex flex-1 flex-col p-12 lg:col-span-6">
             {/* header */}
-            <header>
-                <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>The{' '}<span className='font-extrabold underline decoration-pink-600/50'>iizwiiz</span>{' '}NFT Market Place</h1>
-                <button>Sign In</button>
+            <header className="flex items-center justify-between">
+                <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>The{' '}<span className='font-extrabold underline decoration-pink-600/50 hover:animate-spin'>iizwiiz</span>{' '}NFT Market Place</h1>
+                <button className='rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base hover:shadow-lg cursor-pointer hover:border-none hover:text-rose-400 hover:bg-white transition duration-200 ease-out'>Sign In</button>
             </header>
             
             {/* content */}
