7485f50ae72a5c776cf1fe8efed186eec5648185
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-04-05 12:10:10 -0500

    added conditional operator to switched between sign in and sign out if theres a waller address

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -32,9 +32,9 @@ function NFTDropPage() {
             <header className="flex items-center justify-between">
                 <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>The{' '}<span className='font-extrabold underline decoration-pink-600/50'>iizwiiz</span>{' '}NFT Market Place</h1>
                 <button className='rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base hover:shadow-lg cursor-pointer hover:border-none hover:text-rose-400 hover:bg-white transition duration-200 ease-out'
-                        onClick={() => connectWithMetamask()}
+                        onClick={() => address ? disconnect() : connectWithMetamask()}
                 >
-                    Sign In
+                    {address ? 'Sign Out' : 'Sign In'}
                 </button>
             </header>
             <hr className='my-2 border'/>
