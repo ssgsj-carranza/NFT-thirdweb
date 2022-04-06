c40464dc5acfb7469cc6945d11d0d5ec1ebecd1e
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-04-05 22:51:31 -0500

    added welcome page with link to main page once clicked, added styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
+
 
 const Home: NextPage = () => {
   return (
     <div className="">
       <Head>
-        <title>NFT Drop</title>
+        <title>iizwiiz NFT</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className=''>NFT Drop</h1>
+      <div className="flex h-screen bg-[url('https://www.photofunky.net/output/image/c/6/c/6/c6c6f0/photofunky.gif')] bg-cover bg-center">
+        <div className="m-auto">
+          <Link href='/nft/nft'>
+            <button className='font-bold z-30 text-white hover:rounded-full px-4 py-2 hover:bg-rose-400'
+            >iizwiiz NFT Drop</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
@@ -20,3 +29,8 @@ export default Home
 // npm install html2canvas - uninstalled
 // npm install @thirdweb-dev/react @thirdweb-dev/sdk ethers
 
+{/* <div className="flex h-screen">
+<div className="m-auto">
+<h1 className='font-bold'>iizwiiz NFT Drop</h1>
+</div> */}
+// <h1 className="font-bold items-center">iizwiiz NFT Drop</h1>
