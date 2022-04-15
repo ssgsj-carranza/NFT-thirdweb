321e3c8f3075cb50339321992596866e25e84019
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-04-15 12:53:51 -0500

    pulling in title and description to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import type { NextPage, GetServerSideProps } from 'next'
 import Head from 'next/head'
 import {sanityClient, urlFor} from '../sanity';
 import Link from 'next/link';
+import { Collection } from '../typings';
 
 // describes shape of object
 interface Props {
-  colelctions: Collection[]
+  collections: Collection[]
 }
 
 const Home = ({collections}: Props) => {
@@ -15,14 +16,33 @@ const Home = ({collections}: Props) => {
         <title>iizwiiz NFT</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex h-screen bg-[url('https://www.photofunky.net/output/image/c/6/c/6/c6c6f0/photofunky.gif')] bg-cover bg-center">
-        <div className="m-auto">
-          <Link href='/nft/nft'>
-            <button className=' animate-bounce font-bold rounded-full z-30 text-white px-4 py-2 hover:bg-gradient-to-br from-purple-400 to-yellow-500 hover:rounded-full hover:shadow-lg transition duration-200 ease-out'
-            >iizwiiz NFT Drop</button>
-          </Link>
+      <main>
+        <div className="flex h-screen bg-[url('https://www.photofunky.net/output/image/c/6/c/6/c6c6f0/photofunky.gif')] bg-cover bg-center">
+          <div className="m-auto">
+            <div>
+              {collections.map((collection) => (
+                <>
+                  <div>
+                    <img className='h-80 w-96 rounded-2xl object-cover' 
+                        src={urlFor(collection.mainImage).url()}
+                        alt=''
+                    />
+                  </div>
+
+                  <div>
+                    <h2 className='text-3xl'>{collection.title}</h2>
+                    <p className='mt-2 text-sm text-white'>{collection.description}</p>
+                  </div>
+                </>
+              ))}
+            </div>
+            <Link href='/nft/nft'>
+              <button className=' animate-bounce font-bold rounded-full z-30 text-white px-4 py-2 hover:bg-gradient-to-br from-purple-400 to-yellow-500 hover:rounded-full hover:shadow-lg transition duration-200 ease-out'
+              >iizwiiz NFT Drop</button>
+            </Link>
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   )
 }
